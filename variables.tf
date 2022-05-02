e17cf1cfa9cd46772c131afd3b8046b1797c50e1
Diego Coral <diego.corral@walmart.com>
2022-05-02 17:36:52 -0400

    destroy cluster

--- variables.tf
+++ variables.tf
@@ -17,16 +17,3 @@ variable "initial_node_count" {
 variable "machine_type" {
   default = "n1-standard-2"
 }
-
-# ##credentials for kubernetes username and password
-# variable "clusterusername" {
-# #type = "string"
-# description = "User name for authentication to the Kubernetes linux agent virtual machines in the cluster."
-# default = "dcc12345"
-# }
-
-# variable "clusterpassword" {
-# #type ="string"
-# default = "MySecretPassTest123."
-# description = "The password for the Linux admin account."
-# }
