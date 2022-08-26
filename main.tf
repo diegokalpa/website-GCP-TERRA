d13335f3a0ddb5c2d2e40460ceda6b82e712f564
diegocalpa <diegocalpa@MACBOOKAIR-423C.local>
2022-08-26 11:01:08 -0400

    PLAN only for single nodes

--- main.tf
+++ main.tf
@@ -20,19 +20,19 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
-  cluster    = google_container_cluster.primary.id
-  node_count = 1
+# resource "google_container_node_pool" "primary_preemptible_nodes" {
+#   name       = "my-node-pool"
+#   cluster    = google_container_cluster.primary.id
+#   node_count = 1
 
-  node_config {
-    preemptible  = true
-    machine_type = "e2-small"
+#   node_config {
+#     preemptible  = true
+#     machine_type = "e2-small"
 
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    #service_account = google_service_account.default.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#     #service_account = google_service_account.default.email
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/cloud-platform"
+#     ]
+#   }
+# }
